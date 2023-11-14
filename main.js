90740de8ed69d3f0a5e2e157fefa0f42ddee3e2d
lokih1 <lokihya@gmail.com>
2023-11-14 17:56:06 -0300

    Operador logico

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
+const telaTel = document.querySelector("input[type=tel]");
+const listaTel = document.querySelectorAll("input[type=button]");
 
-const telaTel = document.querySelector('input[type=tel]');
-const listaTel = document.querySelectorAll('input[type=button]');
+for (let contador = 0; contador < listaTel.length; contador++) {
+  const tecla = listaTel[contador];
 
-for (let contador = 0; contador < listaTel.length; contador++ ) {
-    const tecla = listaTel[contador];
-    
-    tecla.onclick = function () {
-        telaTel.value += tecla.value;
+  tecla.onclick = function () {
+    telaTel.value += tecla.value;
+  };
+  tecla.onkeydown = function (e) {
+    if (e.code === "Space" || e.code === "Enter") {
+      tecla.classList.add("ativa");
     }
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
-    }
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
-
